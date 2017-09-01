10c1420c038c484c95af7dc786fb63e399ede576
Zollak <zollak@thecorporateer.com>
2017-09-01 02:17:05 +0200

    started work on influence details

--- influence-system-app/src/app/influence-details/influence-details.component.ts
+++ influence-system-app/src/app/influence-details/influence-details.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Http, Headers, Response } from '@angular/http';
+
+import { ObjectService } from '../services/object.service';
+import { AuthService } from '../services/auth.service';
+import { CorporateerService } from '../services/corporateer.service';
+import { Influence } from '../influence';
 
 @Component({
   selector: 'app-influence-details',
   templateUrl: './influence-details.component.html',
   styleUrls: ['./influence-details.component.css']
 })
-export class InfluenceDetailsComponent {
+export class InfluenceDetailsComponent implements OnInit {
   public isCollapsed = true;
+
+  currentInfluence: Influence[];
+
+  constructor(private http: Http, private authService: AuthService, private objectService: ObjectService, private corporateerService: CorporateerService) {
+
+  }
+
+  ngOnInit() {
+
+  }
 }
