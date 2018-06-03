f84451ada9ae06e6eaa7423ff1a39646f15fb55f
Zollak <zollak@thecorporateer.com>
2018-06-03 23:00:11 +0200

    removed rank buying, changed API possibilities

--- influence-system-app/src/app/app-settings.ts
+++ influence-system-app/src/app/app-settings.ts
@@ -1,4 +1,5 @@
 export class AppSettings {
-    public static API='https://api.influence-system.tk:8443/';
-    /** public static API='http://localhost:8443/'; */
+    /**public static API='https://api.influence-system.tk:8443/';*/
+    public static API='http://localhost:8443/';
+    /** public static API='https://influencesysapi.thecorporateer.com/';*/
 }
